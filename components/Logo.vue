241933eacda5ddc711f9492aaa6d5e1a74c2921f
Pedro Tavares <pedro@pedrotavares.com>
2019-04-30 16:34:47 +0100

    Remove CSS animations as they conflict with our GSAP code.

--- components/Logo.vue
+++ components/Logo.vue
@@ -10,7 +10,6 @@
 <style>
 .VueToNuxtLogo {
   display: inline-block;
-  animation: turn 2s linear forwards 1s;
   transform: rotateX(180deg);
   position: relative;
   overflow: hidden;
@@ -35,7 +34,6 @@
 .Triangle--two {
   top: 30px;
   left: 35px;
-  animation: goright 0.5s linear forwards 3.5s;
   border-left: 87.5px solid transparent;
   border-right: 87.5px solid transparent;
   border-bottom: 150px solid #3b8070;
@@ -44,7 +42,6 @@
 .Triangle--three {
   top: 60px;
   left: 35px;
-  animation: goright 0.5s linear forwards 3.5s;
   border-left: 70px solid transparent;
   border-right: 70px solid transparent;
   border-bottom: 120px solid #35495e;
@@ -53,27 +50,8 @@
 .Triangle--four {
   top: 120px;
   left: 70px;
-  animation: godown 0.5s linear forwards 3s;
   border-left: 35px solid transparent;
   border-right: 35px solid transparent;
   border-bottom: 60px solid #fff;
-}
-
-@keyframes turn {
-  100% {
-    transform: rotateX(0deg);
-  }
-}
-
-@keyframes godown {
-  100% {
-    top: 180px;
-  }
-}
-
-@keyframes goright {
-  100% {
-    left: 70px;
-  }
 }
 </style>
