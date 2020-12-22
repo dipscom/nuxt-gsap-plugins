2fdd0b59c923e9a3fd393814b844f34b2bbeba1d
Pedro Medeiros Tavares <pedro@pedrotavares.com>
2020-12-22 10:28:08 +0000

    Update sample code

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,84 +1,16 @@
-import pkg from './package'
-
 export default {
-  mode: 'universal',
-
-  /*
-  ** Headers of the page
-  */
+  target: 'static',
   head: {
-    title: pkg.name,
-    meta: [
-      { charset: 'utf-8' },
-      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: pkg.description }
-    ],
-    link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
-    ],
-    /*
-      If you do not have any particular reason to bundle
-      GSAP in your project, you can easily add links to the
-      various CDNs and enjoy all the benefits it brings
-    */
-    // script: [
-    //   {
-    //     src: 'https://cdnjs.cloudflare.com/ajax/libs/gsap/2.1.2/TweenMax.min.js'
-    //   },
-    //   {
-    //     src: 'https://cdnjs.cloudflare.com/ajax/libs/gsap/2.1.2/utils/Draggable.min.js'
-    //   },
-    //   // Member-only plugin that exists in the ./static folder (NOT present in this repository)
-    //   {
-    //     src: '/ThrowPropsPlugin.min.js'
-    //   },
-    //   {
-    //     src: '/GSDevTools.min.js'
-    //   },
-    //   {
-    //     src: '/SplitText.min.js'
-    //   },
-    // ]
+    script: [
+      {
+        src: 'https://cdnjs.cloudflare.com/ajax/libs/gsap/3.5.1/gsap.min.js',
+      },
+      {
+        src: 'https://cdnjs.cloudflare.com/ajax/libs/gsap/3.5.1/Draggable.min.js'
+      }
+    ]
   },
-
-  /*
-  ** Customize the progress-bar color
-  */
-  loading: { color: '#fff' },
-
-  /*
-  ** Global CSS
-  */
   css: [
+    '~/assets/globals.css',
   ],
-
-  /*
-  ** Plugins to load before mounting the App
-  */
-  plugins: [
-  ],
-
-  /*
-  ** Nuxt.js modules
-  */
-  modules: [
-  ],
-
-  /*
-  ** Build configuration
-  */
-  build: {
-    /*
-    ** You can extend webpack config here
-    */
-    extend(config, ctx) {
-    },
-    /*
-      If you do not want to use the UMD version of GSAP,
-      you'll have to tell Babel to transpile the library and its plugins
-      from ES modules.
-      Bellow is how you would configure it.
-    */
-    // transpile: ['TweenMax', 'Draggable', 'ThrowPropsPlugin', GSDevTools],
-  }
 }
---
+++ pages/draggable.vue
@@ -0,0 +1,29 @@
+<template>
+  <main>
+    <h1>Draggable example</h1>
+    <div class="draggable">Drag me</div>
+    <NuxtLink to="/">
+      Home
+    </NuxtLink>
+  </main>
+</template>
+
+<script>
+export default {
+  mounted: function () {
+    gsap.registerPlugin(Draggable)
+
+    Draggable.create('.draggable')
+  }
+}
+</script>
+
+<style scoped>
+  .draggable {
+    background: white;
+    border: 2px dashed black;
+    padding: 1rem;
+    text-align: center;
+    font-weight: 600;
+  }
+</style>
